601e1feb7d0d4a3845909b3c6fc101a56ae862eb
Jaeden Waller <146506880+Fighterplane2@users.noreply.github.com>
2024-07-03 17:11:40 +0000

    js work and code

---
+++ js/JS_learning.js
@@ -0,0 +1,20 @@
+//          Below is the basic name prompt code
+//let name = prompt("What is your name?");
+// if(name === "jaeden"){
+//     window.alert("Hello Jaeden")
+// } else {
+//     window.alert("Hey there stranger")
+// }
+//          Below is the basic code for the age asker
+// let age = prompt("Please enter your age.");
+// if (age = 18){
+//     window.alert("Wassup Unc?")
+//   }  else{
+//     window.alert("Hello little one.")
+//   }
+//          very very basic password thing
+// let answer = "password"
+// let guess = prompt("Please enter password.")
+// while( answer !== guess){
+//     guess = prompt("Please enter password.")
+// }
--- js/main.js
+++ js/main.js
@@ -3,3 +3,19 @@ $( document ).ready(function() {
         window.alert("DID YOU JUST CLICK ME?!");
     });
 });
+
+
+// let age = prompt("Please enter your age.");
+// age = Number(age);
+
+// if (age < 18){
+//     window.alert("Hey kid.");
+// } else if(age >= 18 && age > 21){
+//     window.alert("Holy you're old..");
+// } else{
+//     window.alert("Wassup Unc.")
+// }
+
+for (let i = 0; i <= 10; i++){
+    console.log(i)
+}
